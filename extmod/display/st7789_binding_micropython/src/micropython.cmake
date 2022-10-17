09e7de589171cbfcbd15af49b6e7637d0d6c9e30
liangyingy <1102390310@qq.com>
2022-10-17 14:25:38 +0800

    feature/st7789_binding_micropython: add st7789 module
    Signed-off-by: liangyingy <1102390310@qq.com>

---
+++ extmod/display/st7789_binding_micropython/src/micropython.cmake
@@ -0,0 +1,3 @@
+set(ST7789_DIR ${CMAKE_BINARY_DIR}/../st7789_binding_micropython)
+
+include(${ST7789_DIR}/st7789/micropython.cmake)
--- extmod/micropython.cmake
+++ extmod/micropython.cmake
@@ -15,6 +15,10 @@ if(EXTMOD_ST7789)
 include(${CMAKE_CURRENT_LIST_DIR}/display/st7789/src/micropython.cmake)
 endif()
 
+if(EXTMOD_ST7789_BINDING_MICROPYTHON)
+include(${CMAKE_CURRENT_LIST_DIR}/display/st7789_binding_micropython/src/micropython.cmake)
+endif()
+
 if(EXTMOD_BMA)
 include(${CMAKE_CURRENT_LIST_DIR}/sensor/bma/src/micropython.cmake)
 endif()
